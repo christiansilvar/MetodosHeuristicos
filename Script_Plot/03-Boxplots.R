6c98f26f25c7a45b5c5e6916169a099b000d6985
christiansilvar <christians.rodrigues@hotmail.com>
2021-10-16 11:40:48 -0300

    Gráficos

---
+++ Script_Plot/03-Boxplots.R
@@ -0,0 +1,31 @@
+# Boxplots
+
+
+?boxplot
+?sleep
+
+# Permite utilizar as colunas sem especificar o nome do dataset
+attach(sleep)
+View(sleep)
+
+# Construção do boxplot
+sleepboxplot = boxplot(data = sleep, extra ~ group,
+                       main = "Duração do Sono",
+                       col.main = "red", ylab = "Horas", xlab = "Droga")
+
+# Cálculo da média
+medias = by(extra, group, mean)
+
+# Adiciona a média ao gráfico
+points(medias, col = "red")
+
+
+# Boxplot horizontal
+horizontalboxplot = boxplot(data = sleep, extra ~ group,
+                            ylab = "", xlab = "", horizontal = T)
+
+horizontalboxplot = boxplot(data = sleep, extra ~ group,
+                            ylab = "", xlab = "", horizontal = T,
+                            col = c("blue", "red") )
+
+
